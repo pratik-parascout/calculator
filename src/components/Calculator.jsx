515d79451a7a70b288bfacf3df505fbd1d351552
Pratik Kumar <pratikumar56@gmail.com>
2024-05-27 14:00:07 +0530

    completed most of the calculator

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -4,33 +4,77 @@ import KeyWindow from "./KeyWindow"
 
 const Calculator = () => {
 
-    const [expression, setExpression] = useState('')
-    const [displayExp, setDisplayExp] = useState('')
-    const [result, setResult] = useState('0')
+     const [expression, setExpression] = useState('')
+     const [displayEXP, setDisplayEXP] = useState('')
+     const [result, setResult] = useState('0')
 
-    function handleButton(value){
-        // console.log(value);
-        if(value==="AC"){
-            setDisplayExp("")
-            setExpression("")
-            setResult("0")
-        }
-        else if (value==="DEL"){
-            setDisplayExp(displayExp.slice(0,-1))
-            setExpression(expression.slice(0,-1))
-        }
-        else{
-            setExpression(expression+value)
-            setDisplayExp(displayExp+value)
-        }
-    }
+     const sciFunc = {
+       sin: 'Math.sin',
+       cos: 'Math.cos',
+       tan: 'Math.tan',
+       ln: 'Math.log',
+       log: 'Math.log10',
+       π: 'Math.PI',
+       e: 'Math.E',
+       '^': '**',
+       '√': 'Math.sqrt',
+     }
 
-  return (
-    <div className=" border-solid border-4 border-textColor border-opacity-5 rounded-lg max-w-128 w-fit overflow-hidden">
-        <DisplayWindow expression={displayExp} result={result} />
-        <KeyWindow handleButton={handleButton}/>
+     function calcResult() {
+       if (expression.length !== 0) {
+         try {
+           let compute = eval(expression)
+           compute = parseFloat(compute.toFixed(4))
+           setResult(compute)
+         } catch (error) {
+           setResult('An Error Occurred!')
+         }
+       } else {
+         setResult('An Error Occurred!')
+       }
+     }
 
-    </div>
-  )
+     function handleButton(value) {
+       if (value === 'AC') {
+         setExpression('')
+         setDisplayEXP('')
+         setResult('0')
+       } else if (value === 'DEL') {
+         setDisplayEXP(displayEXP.slice(0, -1))
+         setExpression(expression.slice(0, -1))
+       } else if (sciFunc.hasOwnProperty(value)) {
+         setDisplayEXP(displayEXP + value)
+         setExpression(expression + sciFunc[value])
+       } else if (value === '!') {
+         const lastNum = extractLastNum(expression)
+         if (lastNum != null) {
+           const num = parseFloat(lastNum)
+           setDisplayEXP(displayEXP + value)
+           setExpression(expression.replace(lastNum, factorial(num)))
+         }
+       } else if (value === '=') calcResult()
+       else {
+         setExpression(expression + value)
+         setDisplayEXP(displayEXP + value)
+       }
+     }
+
+     function factorial(n) {
+       let result = 1
+       for (let i = 1; i <= n; i++) result *= i
+       return result
+     }
+
+     function extractLastNum(exp) {
+       const numbers = exp.match(/\d+/g)
+       return numbers ? numbers[numbers.length - 1] : null
+     }
+
+     return (
+       <div className="calculator">
+         <DisplayWindow expression={displayEXP} result={result} />
+         <KeyWindow handleButton={handleButton} />
+       </div>
+     )
 }
 export default Calculator
